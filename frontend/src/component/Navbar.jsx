e21ff58da71211579c905175b6128e2d8a459bfa
dat <nguyentuandat1603@gmail.com>
2024-05-19 16:17:47 +0700

    /a

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -8,11 +8,11 @@ function Navbar(){
     return <>
         <nav className="navbar navbar-expand-sm bg-light">
             <div className="container-fluid">
-                <Link to="/" style={item}>
-                <a className="navbar-brand" >
+                <Link to="/" style={item}  className="navbar-brand">
+
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/CapShield05.jpg/220px-CapShield05.jpg" width="30" height="30" className="d-inline-block align-top"/>
                     Security
-                </a>
+
                 </Link>
                 <div className="nav justify-content-end">
                             <Link to="/login" style={item}>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <GoogleOAuthProvider clientId="261703293557-0aa1tvmududgbosur0o3dleljfpsb90h.apps.googleusercontent.com">
     <App />
+      </GoogleOAuthProvider>
   </React.StrictMode>,
 )
